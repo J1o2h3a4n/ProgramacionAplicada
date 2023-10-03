d357bd1d62fee2f1ef25897ec600557875453bf7
J1o2h3a4n <142907931+J1o2h3a4n@users.noreply.github.com>
2023-10-03 09:12:45 -0500

    Create Diccionario4.py

---
+++ SegundoCorte/EstructuraDeDatos/Diccionarios/Diccionario4.py
@@ -0,0 +1,14 @@
+
+diccionario={"Nombre":"Johan","Apellido":"Forero"}.get("Nombre")#Devuelve unicamente el valor de la palabra clave seleccionado.
+#Si en la funcion get() la palabra clave no esta presente en el diccionario, se arrojara un valor vacio:none
+print(diccionario)
+
+dict1={'color': 'blue', 'shape': 'circle'} 
+dict2={'color':'red', 'number':42}
+print("before: ",dict1)
+dict1.update(dict2)
+print("after: ", dict1) #el diccionario 2 actualiza al diccionario 1, la forma se conseva
+
+new_dict={"A":"Ave","T":"Tucan","V":"Vaca"}
+print(new_dict.keys())
+print(new_dict.items())
